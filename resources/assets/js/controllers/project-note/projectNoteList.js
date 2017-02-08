6d4dff167fb602872f040f6714e66fb33e2fe509
Iuri Cristofaro <iuri@iuricristofaro.com.br>
2017-02-08 16:20:09 -0200

    projectNodeList.js

--- resources/assets/js/controllers/project-note/projectNoteList.js
+++ resources/assets/js/controllers/project-note/projectNoteList.js
@@ -1,11 +1,8 @@
 angular.module('app.controllers')
-    .controller('ProjectNoteListController', ['$scope','$routeParams','$location','Notification','ProjectNote',
-        function($scope,$routeParams,$location,Notification,ProjectNote){
+    .controller('ProjectNoteListController', 
+        ['$scope','$routeParams','ProjectNote',
+        function($scope,$routeParams,ProjectNote){
         $scope.projectNotes = ProjectNote.query({id: $routeParams.id},function(){
-        },function(error){
-            if(error.data.hasOwnProperty('error') && error.data.error){
-                Notification.error(error.data.message);
-                $location.path('/projects/dashboard');
-            }
+        
         });
     }]);
